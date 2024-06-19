15f5b69bccc3e183dac5db1f9bd5209a323c74ae
develop <dofastsite@gmail.com>
2024-06-20 02:15:16 +0300

    Complete problem 2

--- leetcode/2. Add Two Numbers.py
+++ leetcode/2. Add Two Numbers.py
@@ -10,20 +10,20 @@ class ListNode:
 
 class Solution:
     def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy_head = ListNode(0)
-        curr = dummy_head
+        init_head = ListNode()
+        current_node = init_head
         carry = 0
         while l1 is not None or l2 is not None or carry != 0:
-            l1_value = l1.val if l1 else 0
-            l2_value = l2.val if l2 else 0
-            column_sum = l1_value + l2_value + carry
-            carry = column_sum // 10
-            new_node = ListNode(column_sum % 10)
-            curr.next = new_node
-            curr = new_node
+            value_1 = l1.val if l1 else 0
+            value_2 = l2.val if l2 else 0
+            sum_value = value_1 + value_2 + carry
+            carry = sum_value // 10
+            node = ListNode(sum_value % 10)
+            current_node.next = node
+            current_node = node
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        return dummy_head.next
+        return init_head.next
 
 
 node_1 = ListNode(2)
@@ -37,3 +37,32 @@ node_2.next.next = ListNode(4)
 solution = Solution()
 s = solution.add_two_numbers(node_1, node_2)
 print([s.val, s.next.val, s.next.next.val])
+
+
+node_1 = ListNode(9)
+node_1.next = ListNode(9)
+node_1.next.next = ListNode(9)
+node_1.next.next.next = ListNode(9)
+node_1.next.next.next.next = ListNode(9)
+node_1.next.next.next.next.next = ListNode(9)
+node_1.next.next.next.next.next.next = ListNode(9)
+
+node_2 = ListNode(9)
+node_2.next = ListNode(9)
+node_2.next.next = ListNode(9)
+node_2.next.next.next = ListNode(9)
+
+solution = Solution()
+s = solution.add_two_numbers(node_1, node_2)
+print(
+    [
+        s.val,
+        s.next.val,
+        s.next.next.val,
+        s.next.next.next.val,
+        s.next.next.next.next.val,
+        s.next.next.next.next.next.val,
+        s.next.next.next.next.next.next.val,
+        s.next.next.next.next.next.next.next.val,
+    ]
+)
